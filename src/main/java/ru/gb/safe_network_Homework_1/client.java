5dd98d0de63cade61778a974ad4f9b2023a67ef1
SGoose <dragonandnaruto@gmail.com>
2022-04-18 19:49:53 +0300

    TEST 1

---
+++ src/main/java/ru/gb/safe_network_Homework_1/client.java
@@ -0,0 +1,4 @@
+package ru.gb.safe_network_Homework_1;
+
+public class client {
+}
---
+++ src/main/java/ru/gb/safe_network_Homework_1/server.java
@@ -0,0 +1,7 @@
+package ru.gb.safe_network_Homework_1;
+
+public class server {
+    public static void main(String[] args) {
+
+    }
+}
